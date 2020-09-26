99616efdfd000a9b4f99f419c05b6916af19b99e
jchan3990 <jonchan3990@gmail.com>
2020-09-26 16:06:09 -0700

    Can highlight selected text

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -12,25 +12,29 @@ const App = () => {
 
   useEffect(() => {
     let hp = sampleData.split('\n\n\n');
-    setPassage(hp.map((pass, idx) => <p key={idx} id={idx}>{pass}</p>));
+    setPassage(hp.map((pass, idx) => <p key={idx} id={idx} className="passage">{pass}</p>));
   }, [])
 
   const selectPhrase = (event) => {
-    // event.preventDefault();
+    event.preventDefault();
     let selObj = window.getSelection();
     let passId = Number(selObj.anchorNode.parentNode.id);
     let start = selObj.anchorOffset;
     let end = selObj.extentOffset;
-    let string = passage[passId].props.children.slice(start, end);
+    let string = selObj.toString();
+    let toChange = document.getElementsByClassName("passage")[passId].innerHTML
+    console.log(string);
+    console.log(toChange);
+    toChange = toChange.replace(string, "<span class=highlighted>" + string + "</span>");
+    document.getElementsByClassName("passage")[passId].innerHTML = toChange;
+    console.log(toChange);
   }
 
   return (
     <div className="main">
       <TopBar />
-      <div className="passageContainer">
-        <span className="passage" onClick={selectPhrase}>
-          {passage}
-        </span>
+      <div className="passageContainer" onMouseUp={selectPhrase}>
+        {passage}
       </div>
     </div>
   )
